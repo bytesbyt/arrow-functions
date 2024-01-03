4442b5eaea802b85915f18a744702f4a72436473
Tes <101018180+bytesbyt@users.noreply.github.com>
2024-01-03 15:33:43 +0000

    Add ES6 Arrow Functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -13,10 +13,29 @@ console.log(sum)
 
 
 // Arrow Function With Parameters
-
+const addTwoNumbers1 = (a, b) => {
+    //Code block
+    return a + b;
+}
+let sum1 = addTwoNumbers1(3, 5);
+console.log(sum1);
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b); also valid
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers(6, 4);
+console.log(sum2);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!');
+
+const sayHello = () => console.log('hello');
+sayHello();
 
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    '<p>This a multipline string!</p>'
+)
+console.log(returnMultipleLines());
